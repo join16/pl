e205b44c5ee775f35bc07276239659143392c93f
join <choin1116@hanmail.net>
2014-11-17 01:05:03 +0900

    Init

---
+++ src/com/example/device/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.device;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.devicecontroller.DeviceController;
+
+public class MainActivity extends ActionBarActivity {
+
+	private DeviceController mDevice;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDevice = new DeviceController(getApplicationContext());
+    }
+
+    public void mOnClick(View v) {
+    	switch(v.getId()) {
+    	case R.id.wifi:
+    		mDevice.switchWifi(true);
+    		break;
+    	case R.id.bluetooth:
+    		mDevice.switchBluetooth(true);
+    		break;
+    	case R.id.network:
+    		mDevice.switchNetwork(true);
+    		break;
+    	case R.id.sync:
+    		mDevice.switchSync(true);
+    	}
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ src/com/example/devicecontroller/DeviceController.java
@@ -0,0 +1,77 @@
+package com.example.devicecontroller;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
+import android.provider.Settings;
+import android.util.Log;
+
+public class DeviceController {
+	
+	private Context mContext;
+	
+	final private WifiManager mWifi;
+	private String gpsSetting;
+	final private BluetoothAdapter mBluetooth;
+	final private ConnectivityManager mNetwork;
+	
+	/*----------- constructor -----------*/
+	public DeviceController(Context _mContext) {
+		mContext = _mContext;
+		mWifi = (WifiManager) mContext.getSystemService (Context.WIFI_SERVICE);
+		gpsSetting = null;
+		mBluetooth = BluetoothAdapter.getDefaultAdapter();
+		mNetwork = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+	}
+	
+	public void switchWifi(boolean isEnable) {  // true => wi-fi on   false => wi-fi off
+		mWifi.setWifiEnabled(isEnable);
+	}
+	public void switchGps(boolean isEnable) {  /* ....need system root permissions....*/
+		Log.e("test", "function entered");
+		if (isEnable) {
+		    gpsSetting = Settings.Secure.getString(mContext.getContentResolver(),
+                    Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+			String newSet = String.format("%s,%s", gpsSetting, LocationManager.GPS_PROVIDER);
+			try {
+			Settings.Secure.putString(mContext.getContentResolver(),
+			         Settings.Secure.LOCATION_PROVIDERS_ALLOWED,
+			         newSet);
+			} catch(Exception e) {
+				Log.d("exception error", e.toString());
+			}
+		}
+	}
+	public void switchBluetooth(boolean isEnable) {
+		if (isEnable) {
+			mBluetooth.enable();
+		} else {
+			mBluetooth.disable();
+		}
+	}
+	public void switchNetwork(boolean isEnable) {
+		try {
+		    final Class mNetworkClass = Class.forName(mNetwork.getClass().getName());
+		    final Field iConnectivityManagerField = mNetworkClass.getDeclaredField("mService");
+		    iConnectivityManagerField.setAccessible(true);
+		    final Object iConnectivityManager = iConnectivityManagerField.get(mNetwork);
+		    final Class iConnectivityManagerClass = Class.forName(
+		        iConnectivityManager.getClass().getName());
+		    final Method setMobileDataEnabledMethod = iConnectivityManagerClass
+		        .getDeclaredMethod("setMobileDataEnabled", Boolean.TYPE);
+		    setMobileDataEnabledMethod.setAccessible(true);
+		    setMobileDataEnabledMethod.invoke(iConnectivityManager, isEnable);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}	
+	public void switchSync(boolean isEnable) {
+		ContentResolver.setMasterSyncAutomatically(isEnable);
+	}
+}
